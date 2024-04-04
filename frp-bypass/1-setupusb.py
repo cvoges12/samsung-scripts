8111292ab422b1def4a2666cdcdfb5f572555109
Inkstray <80412260+inkstray@users.noreply.github.com>
2024-04-04 20:11:33 +0200

    make usb setup script more verbose

--- frp-bypass/1-setupusb.py
+++ frp-bypass/1-setupusb.py
@@ -1,14 +1,26 @@
-# setup USB
 import usb.core
-dev = usb.core.find(idVendor=0x04e8, idProduct=0x6860) 
-dev.reset()
 
-# First time somehow doesnt work
+# set ur values here
+VID=0x04e8
+PID=0x6860
+usbcfg = 0x2
+
+# look for USB device
+dev = usb.core.find(idVendor=VID, idProduct=PID)
+
+# first time will always fail. (i have no idea why)
+print("[1/2] Resetting USB device and setting configuration value...")
+dev.reset()
 try:
-    dev.set_configuration(0x2)
+    dev.set_configuration(usbcfg)
 except Exception:
+    print("  !! Task failed successfully.\n  :: This should be completely normal, dont ask me why...")
     pass
 
-# Second time it will
+# second time's the charm.
+print("[2/2] Trying again... (No output if it succeeds)")
 dev.reset()
-dev.set_configuration(0x2)
+try:
+    dev.set_configuration(usbcfg)
+except Exception:
+    print("  !! Failed to set configuration...\n  :: Value is already set or it actually failed...\n")
